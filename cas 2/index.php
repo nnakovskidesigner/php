d0f84e4182f5562b0325783a3be72b47c4cd0be6
Semos Akademija <akademija@semosedu.local>
2023-05-04 18:58:10 +0200

    updatE

---
+++ cas 2/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+
+<h1>Ova e naslov preku html</h1>
+    
+<h1><?php echo "<h1>h11111</h1>"; ?></h1>
+
+<?php 
+    $text = "ova e naslov preku php";
+    $x = 5;
+    $y = 30;
+    $zbir = $x + $y;
+    $t = 101.5;
+?>
+
+<p><?php echo $x + $y;?> e isto shto i <?php echo $zbir;?></p>
+
+<?php
+    echo $text;
+?>
+
+<?php echo "<h1>h11111</h1>"; ?>
+    
+<p><?php echo ($x + $y)." e isto kako i ".$zbir;?></p>
+
+<?php echo "<h1>".$text."</h1>"; ?>
+    
+</body>
+</html>
+
+<?php 
+echo "ova e naseto prvo echo";
+
+
+// single line comment 
+#single line
+/*multi 
+line comment
+*/
+
+
+//variables
+
+
+
+
+
+
+
+
+?>
