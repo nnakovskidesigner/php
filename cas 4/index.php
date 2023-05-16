6a0204f8ab6a64e0f5eb738a362aa54dc656aac2
Semos Akademija <akademija@semosedu.local>
2023-05-16 19:09:39 +0200

    update

---
+++ cas 4/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+
+
+// echo 4+4;
+
+// $x = 6;
+// $y = 5;
+
+// if ($x > $y) {
+//     echo 'x is greater than y';
+// } else {
+//     echo 'y is greater than or equal to x';
+// }
+
+// x = y
+
+// x += y -> x = x + y
+// x -= y -> x = x - y
+// x *= y -> x = x * y
+// x /= y -> x = x / y
+// x %= y -> x = x % y
+
+
+// ++$c => $c = $c + 1 (increments c, returns $c)
+// $c++ => $c = $c + 1 (returns $c, incremeents $c by 1)
+// $c = 5; 
+// echo ++$c;
+// echo $c;
+// echo $c++; 
+// echo $c; 
+
+
+// $c = 5; 
+// echo --$c; //4
+// echo $c; //4
+// echo $c--; //4 but it makes it now //3
+// echo $c; //3
+
+// String
+// .
+// .=
+
+// $var1 = 'sakam ';
+// $var2 = "chips";
+// echo $var1.$var2;
+// echo $var1.=$var2;
+
+//skripta promenliva a, vrednost da ima 4, prom b vrednost string 'tri',
+// cetri drugi razlicni promenlivi so artimeticki operacii ivresnota da se isnprinta
+
+// $vara = 4;
+// $vara = 'dva';
+// $varb = 'tri';
+
+// echo $vara.$varb;
+// echo $vara + $varb;
+// echo $vara - $varb;
+// echo $vara / $varb;
+
+//za doma php site da najdeme dali vo manuel dali vo net, da vidime shto mozeme da pravime na nivo na operatori pomegju razlicni data tipovi, 
+
+
+
+
+// $broj_na_bukvi = strlen($varb);
+// echo $broj_na_bukvi;
+
+$zbor = 'emilija';
+$recenica = 'moze da se zeme emilija';
+
+$savestrpos = strpos($recenica, $zbor);
+echo $savestrpos;
+
+
+
+echo strrev("Nikola");
+
+
+
+?>
